83bfda6a927241f6a74610f11b0617fdd2171c98
María Jesús Fuenzalida <maria.fuenzalida.serrano@gmail.com>
2020-06-10 14:08:52 -0400

    Primer avance con Nuxt

---
+++ pages/inspire.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img
+        src="https://www.cardcaptorslatinos.com/img/profiles/mari%20clow17209081_cardcaptor-sakura-clear-card-hen-large_4819f7ea_m.jpg"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
